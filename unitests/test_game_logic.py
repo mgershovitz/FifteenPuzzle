7fada376c4992f3f73b0721240e461a41c95802e
Maya Gershovitz <maya@singular.net>
2019-06-30 16:17:07 +0300

    game_logic: very basic moves test

--- unitests/test_game_logic.py
+++ unitests/test_game_logic.py
@@ -1,15 +1,25 @@
+import copy
 import unittest
 
 from common import consts
 from game_logic import PuzzleGame
 from settings import SettingsManager
 
-WINNING_BOARD = [[1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, consts.EMPTY_STR]]
-LOSING_BOARD = [[1, 2, 3],
-                [4, 5, 6],
-                [7, consts.EMPTY_STR, 8]]
+COMPLETED_BOARD = [[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, consts.EMPTY_STR]]
+ALMOST_COMPLETED_BOARD = [[1, 2, 3],
+                          [4, 5, 6],
+                          [7, consts.EMPTY_STR, 8]]
+
+
+def get_completed_board():
+    return copy.deepcopy(COMPLETED_BOARD)
+
+
+def get_almost_complete_board():
+    return copy.deepcopy(ALMOST_COMPLETED_BOARD)
+
 
 class TestGameBoard(unittest.TestCase):
     def setUp(self):
@@ -17,9 +27,19 @@ class TestGameBoard(unittest.TestCase):
         self.test_settings = SettingsManager()
         self.test_settings.load_settings()
 
-    def test_game_won(self):
-        self.tester.init_game(self.test_settings, WINNING_BOARD)
+    def test_initial_boards_game_won(self):
+        self.tester.init_game(self.test_settings, get_completed_board())
         assert self.tester.game_won()
 
-        self.tester.init_game(self.test_settings, LOSING_BOARD)
+        self.tester.init_game(self.test_settings, get_almost_complete_board())
+        assert not self.tester.game_won()
+
+    def test_game_won_with_move(self):
+        self.tester.init_game(self.test_settings, get_almost_complete_board())
+        assert not self.tester.game_won()
+        self.tester.move(consts.LEFT)
+        assert self.tester.game_won()
+        self.tester.move(consts.RIGHT)
+        assert not self.tester.game_won()
+        self.tester.move(consts.RIGHT)
         assert not self.tester.game_won()
